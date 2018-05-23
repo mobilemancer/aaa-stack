e2134559ff5c5ac83b6579116b04a05ce104c1b3
Andreas Wänqvist <andrkarl@gmail.com>
2018-05-23 14:36:09 +0200

    Templating

--- templating/src/app.ts
+++ templating/src/app.ts
@@ -1,3 +1,37 @@
+import { PLATFORM } from "aurelia-pal";
+import { RouterConfiguration, Router } from "aurelia-router";
+
 export class App {
-  message = 'Hello World!';
+  message = "Templating";
+  router: Router;
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = "Title";
+    config.map([
+      {
+        route: "1",
+        name: "one",
+        moduleId: PLATFORM.moduleName("one"),
+        title: "Basic Templating"
+      },
+      {
+        route: "2",
+        name: "two",
+        moduleId: PLATFORM.moduleName("two"),
+        title: "Template with View-Model"
+      },
+      {
+        route: "3",
+        name: "three",
+        moduleId: PLATFORM.moduleName("three"),
+        title: "Template with Conditionals"
+      },
+      {
+        route: "4",
+        name: "four",
+        moduleId: PLATFORM.moduleName("four"),
+        title: "Template with Repeaters"
+      }
+    ]);
+  }
 }
--- templating/src/main.ts
+++ templating/src/main.ts
@@ -1,9 +1,9 @@
 /// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
 // we want font-awesome to load as soon as possible to show the fa-spinner
-import {Aurelia} from 'aurelia-framework'
-import environment from './environment';
-import {PLATFORM} from 'aurelia-pal';
-import * as Bluebird from 'bluebird';
+import { Aurelia } from "aurelia-framework";
+import environment from "./environment";
+import { PLATFORM } from "aurelia-pal";
+import * as Bluebird from "bluebird";
 
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
 Bluebird.config({ warnings: { wForgottenReturn: false } });
@@ -11,7 +11,7 @@ Bluebird.config({ warnings: { wForgottenReturn: false } });
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    .feature(PLATFORM.moduleName("resources/index"));
 
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
@@ -25,8 +25,8 @@ export function configure(aurelia: Aurelia) {
   }
 
   if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    aurelia.use.plugin(PLATFORM.moduleName("aurelia-testing"));
   }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName("app")));
 }
