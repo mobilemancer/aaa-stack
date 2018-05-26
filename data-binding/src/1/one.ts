df210b11d2478b675ce44eca097e1a96ec482e06
Andreas Wänqvist <andrkarl@gmail.com>
2018-05-26 09:36:17 +0200

    Data-binding formated

--- data-binding/src/1/one.ts
+++ data-binding/src/1/one.ts
@@ -1,3 +1,26 @@
 export class One {
   name = "";
+
+  counter_onetime = 0;
+  counter_toview = 0;
+  counter_fromview = 0;
+  counter_twoway = 0;
+  counter_bind = 0;
+
+  click_handler_add() {
+    console.log("adding");
+    this.counter_onetime++;
+    this.counter_toview++;
+    this.counter_fromview++;
+    this.counter_twoway++;
+    this.counter_bind++;
+  }
+
+  click_handler_subract() {
+    this.counter_onetime--;
+    this.counter_toview--;
+    this.counter_fromview--;
+    this.counter_twoway--;
+    this.counter_bind--;
+  }
 }
